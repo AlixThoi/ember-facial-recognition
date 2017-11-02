5436c1f4789b032f5a74ef9a40befd2f0029153d
Alix Thoi <athoi@sdcounty.ca.gov>
2017-11-02 15:11:13 -0700

    fixed ui blue screen

---
+++ app/templates/components/facial-recognition.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-facial-recognition/templates/components/facial-recognition';
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -21,6 +21,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+        recognition: {
+            serviceUrl: ""
+        }
     }
   };
 
@@ -30,6 +33,12 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+      
+      //May have to change the URL to use the location where you obtained your subscription keys.
+      ENV.APP.recognition.serviceUrl="https://westus.api.cognitive.microsoft.com/face/v1.0";
+      ENV.APP.recognition.detectUrl=ENV.APP.recognition.serviceUrl + "/detect?";
+      ENV.APP.recognition.createFaceListUrl=ENV.APP.recognition.serviceUrl + "/facelists/{faceListId}?";
+      ENV.APP.recognition.addFaceToListUrl=ENV.APP.recognition.serviceUrl + "/facelists/{faceListId}/persistedFaces?"
   }
 
   if (environment === 'test') {
